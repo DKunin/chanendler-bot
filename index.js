8844c243105ead998be8103b6a3f85d743449775
Dmitry Kunin <dkunin@avito.ru>
2017-04-07 11:46:55 +0300

    master:Added magnet link shortener to remember

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ bot.onText(/\/stoic/, msg => {
     bot.sendMessage(chatId, stoicapi.random());
 });
 
+// http://mgnet.me/api/create/?m=magnet:?xt=urn:btih:99944890e765e3163f51a53926c12298c2c07de4&dn=Archer.2009.S08E01.HDTV.x264-SVA&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&format=xml
 // tg://bot_command?command=smth
 
 bot.on('inline_query', response => {
