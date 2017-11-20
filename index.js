8fb41daaac2ccd3bd8b10541bb01ca3a9143c794
Dmitri Kunin <whitemarten@gmail.com>
2017-11-21 00:17:12 +0300

    Added magnet yanker integration

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const magnetShortenerIP = process.env.CITADEL_IP;
 const bot = new TelegramBot(token, { polling: true });
 const stoicapi = require('stoic-api');
 const currency = require('./currency');
-const PirateBay = require('thepiratebay');
 const request = require('superagent');
 
 bot.onText(/\/stoic/, msg => {
@@ -29,51 +28,39 @@ bot.onText(/magnet:\?.+/, (msg, match) => {
         });
 });
 
+bot.onText(/episode (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    request(
+        `http://${magnetShortenerIP}:3739/api/get?url=https://thepiratebay.org/search/${escape(match[0].replace('episode ', ''))}`
+    )
+        .set('Accept', 'application/json')
+        .end((err, body) => {
+            request(
+                `http://${magnetShortenerIP}:3738/api/shorten?url=${escape(body.text)}`
+            )
+                .set('Accept', 'application/json')
+                .end((err, body) => {
+                    bot.sendMessage(
+                        chatId,
+                        `http://${magnetShortenerIP}:3738/api/get?hash=${body.text}`
+                    );
+                });
+        });
+});
+
 bot.onText(/\/sign (.+)/, (msg, match) => {
     const chatId = msg.chat.id;
     const word = match[1];
     if (word) {
-        bot.sendMessage(
-            chatId,
-            `© 1996-2017 Jolanta Lapiak:`
-        );
+        bot.sendMessage(chatId, '© 1996-2017 Jolanta Lapiak:');
         bot.sendMessage(
             chatId,
             `https://www.handspeak.com/word/${word[0]}/${word}.mp4`
         );
     } else {
-        bot.sendMessage(
-            chatId,
-            `nope`
-        );
+        bot.sendMessage(chatId, 'nope');
     }
 });
-
-// bot.on('inline_query', response => {
-//     PirateBay.search(response.query)
-//         .then(results => {
-//             console.log(response, results);
-//             const newResults = results.map(({
-//                 id,
-//                 name,
-//                 seeders,
-//                 leechers,
-//                 magnetLink
-//             }) => {
-//                 return {
-//                     id,
-//                     type: 'article',
-//                     title: `S:${seeders} L:${leechers} T:${name.slice(0, 35)}`,
-//                     input_message_content: {
-//                         // tg://bot_command?command=
-//                         message_text: `/shortenmagnet ${magnetLink}`
-//                     }
-//                 };
-//             });
-//             bot.answerInlineQuery(response.id, newResults);
-//         })
-//         .catch(err => console.log(err));
-// });
 
 bot.onText(/\/conv (\d+) (\w{3}) (\w{3})/, (msg, match) => {
     const chatId = msg.chat.id;
@@ -83,7 +70,3 @@ bot.onText(/\/conv (\d+) (\w{3}) (\w{3})/, (msg, match) => {
 });
 
 bot.sendMessage(myChatId, 'Time to get schwifty!');
-
-// bot.on('message', msg => {
-//     const chatId = msg.chat.id;
-// });
