002028559a0dab3496ac3dc430df73076498cfde
Dmitri Kunin <whitemarten@gmail.com>
2017-11-22 23:17:29 +0300

    Removed extra end

--- utils/request.js
+++ utils/request.js
@@ -3,13 +3,12 @@
 const request = require('superagent');
 
 function asyncPromise(url) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         request(url)
             .set('Accept', 'application/json')
             .end((err, body) => {
                 resolve(body);
-            })
-            .catch(reject);
+            });
     });
 }
 
